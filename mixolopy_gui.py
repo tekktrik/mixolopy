a0683a22c583416e4dbabdad0b0440abcf2d4e89
tekktrik <89490472+tekktrik@users.noreply.github.com>
2021-09-24 21:46:21 -0400

    Add automatic popup to select recipe location

--- mixolopy_gui.py
+++ mixolopy_gui.py
@@ -1,13 +1,40 @@
 import tkinter as tk
 from tkinter import ttk
 import tkinter.scrolledtext as scrolledtext
+from tkinter import filedialog as fd
+from dotenv import load_dotenv, find_dotenv
+import os
 import json
 
 class MixoloPy(tk.Tk):
 
-    super()
-    self.title("MixoloPy")
+    def __init__(self):
     
-    cat_frame = ttk.Frame(master=self)
-    tree = ttk.Treeview(master=self)
-    tree
+        super().__init__()
+        self.title("MixoloPy")
+        
+        self.cat_frame = ttk.Frame(master=self)
+        self.cat_tree = ttk.Treeview(master=self.cat_frame)
+        self.cat_tree.heading("#0", text="Categories", anchor="w")
+        
+        recs_loc = self.getRecipesLocation()
+        self.updateCatTree()
+        
+    def getRecipesLocation(self):
+        pot_recloc = os.path.join(os.path.dirname(__file__), ".env")
+        pot_rec = find_dotenv(pot_recloc)
+        if pot_rec == "":
+            self.selectRecipesLocation()
+        load_dotenv(pot_rec)
+                
+    def selectRecipesLocation(self):
+        recloc = fd.askdirectory(title="Select folder to store recipes...")
+        if recloc != '' and recloc != None:
+            with open('.env', 'w') as new_recloc_file:
+                new_recloc_file.write("RECLOC="+recloc)
+        
+    def updateCatTree(self):
+        pass
+        
+mixpy = MixoloPy()
+mixpy.mainloop()
